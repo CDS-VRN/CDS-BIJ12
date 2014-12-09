428d138242926fe75ea686fd12da3252ae4b7b79
annesongithub <annesongithub@users.noreply.github.com>
2014-12-09 15:18:33 +0100

    Start to generate getters en setters

--- vrn-themes/src/main/java/nl/ipo/cds/etl/theme/vrn/beheer/AbstractGebiedBeheer.java
+++ vrn-themes/src/main/java/nl/ipo/cds/etl/theme/vrn/beheer/AbstractGebiedBeheer.java
@@ -2,6 +2,7 @@ package nl.ipo.cds.etl.theme.vrn.beheer;
 
 import nl.ipo.cds.etl.db.annotation.Column;
 import nl.ipo.cds.etl.db.annotation.Table;
+import nl.ipo.cds.etl.theme.annotation.MappableAttribute;
 import nl.ipo.cds.etl.theme.vrn.AbstractGebied;
 
 @Table(name = "hazard_area", schema = "bron")
@@ -21,6 +22,56 @@ public abstract class AbstractGebiedBeheer extends AbstractGebied {
 
     @Column(name="eenheidnummer")
     private String eenheidnummer;
+
+    @MappableAttribute
+	public String getStatusBeheer() {
+		return statusBeheer;
+	}
+
+    @MappableAttribute
+	public void setStatusBeheer(String statusBeheer) {
+		this.statusBeheer = statusBeheer;
+	}
+
+    @MappableAttribute
+	public String getBeheerpakket() {
+		return beheerpakket;
+	}
+
+    @MappableAttribute
+	public void setBeheerpakket(String beheerpakket) {
+		this.beheerpakket = beheerpakket;
+	}
+
+    @MappableAttribute
+	public String getDoelBeheer() {
+		return doelBeheer;
+	}
+
+    @MappableAttribute
+	public void setDoelBeheer(String doelBeheer) {
+		this.doelBeheer = doelBeheer;
+	}
+
+    @MappableAttribute
+	public String getTypeBeheerder() {
+		return typeBeheerder;
+	}
+
+    @MappableAttribute
+	public void setTypeBeheerder(String typeBeheerder) {
+		this.typeBeheerder = typeBeheerder;
+	}
+
+    @MappableAttribute
+	public String getEenheidnummer() {
+		return eenheidnummer;
+	}
+
+    @MappableAttribute
+	public void setEenheidnummer(String eenheidnummer) {
+		this.eenheidnummer = eenheidnummer;
+	}
 
 
 }
