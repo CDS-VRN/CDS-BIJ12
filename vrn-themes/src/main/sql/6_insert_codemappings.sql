b995a642a115a6df64f85e127ab6d69591dc54c1
Stefan van Wouw <stefan.van.wouw@geodan.nl>
2015-01-14 17:00:14 +0100

    Fix database prefix.

--- vrn-themes/src/main/sql/6_insert_codemappings.sql
+++ vrn-themes/src/main/sql/6_insert_codemappings.sql
@@ -1,12 +1,12 @@
 -- CodelistMapping
 
-INSERT INTO codelistmapping VALUES ('TypeBeheerder', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/typeBeheerder.xml');
-INSERT INTO codelistmapping VALUES ('DoelRealisatie', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/doelRealisatie.xml');
-INSERT INTO codelistmapping VALUES ('BeheerPakket', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/beheerPakket.xml');
-INSERT INTO codelistmapping VALUES ('StatusBeheer', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusBeheer.xml');
-INSERT INTO codelistmapping VALUES ('Bronhouder', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/bronhouder.xml');
-INSERT INTO codelistmapping VALUES ('StatusInrichting', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusIngericht.xml');
-INSERT INTO codelistmapping VALUES ('StatusVerwerving', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusVerwerving.xml');
-INSERT INTO codelistmapping VALUES ('TypeBeheerderEnEigenaar', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/typeBeheerder.xml');
+INSERT INTO manager.codelistmapping VALUES ('TypeBeheerder', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/typeBeheerder.xml');
+INSERT INTO manager.codelistmapping VALUES ('DoelRealisatie', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/doelRealisatie.xml');
+INSERT INTO manager.codelistmapping VALUES ('BeheerPakket', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/beheerPakket.xml');
+INSERT INTO manager.codelistmapping VALUES ('StatusBeheer', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusBeheer.xml');
+INSERT INTO manager.codelistmapping VALUES ('Bronhouder', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/bronhouder.xml');
+INSERT INTO manager.codelistmapping VALUES ('StatusInrichting', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusIngericht.xml');
+INSERT INTO manager.codelistmapping VALUES ('StatusVerwerving', 'https://github.com/CDS-VRN/CDS-BIJ12/blob/master/vrn-themes/src/main/feeds/statusVerwerving.xml');
+INSERT INTO manager.codelistmapping VALUES ('TypeBeheerderEnEigenaar', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/typeBeheerder.xml');
 
 
