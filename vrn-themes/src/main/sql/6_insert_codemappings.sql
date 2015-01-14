02129fa1c2341c04326523e4173c73f8b27ff395
Stefan van Wouw <stefan.van.wouw@geodan.nl>
2015-01-14 16:18:52 +0100

    Codemapping insertion for use by docker.

---
+++ vrn-themes/src/main/sql/6_insert_codemappings.sql
@@ -0,0 +1,10 @@
+-- CodelistMapping
+
+INSERT INTO codelistmapping VALUES ('BeheerPakket', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/beheerPakket.xml');
+INSERT INTO codelistmapping VALUES ('StatusVerwerving', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/statusVerwerving.xml');
+INSERT INTO codelistmapping VALUES ('typeBeheerder', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/typeBeheerder.xml');
+INSERT INTO codelistmapping VALUES ('DoelRealisatie', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/doelRealisatie.xml');
+INSERT INTO codelistmapping VALUES ('TypeBeheerderEnEigenaar', '');
+INSERT INTO codelistmapping VALUES ('StatusInrichting', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/statusIngericht.xml');
+INSERT INTO codelistmapping VALUES ('StatusBeheer', 'https://raw.githubusercontent.com/CDS-VRN/CDS-BIJ12/master/vrn-themes/src/main/feeds/statusBeheer.xml');
+
