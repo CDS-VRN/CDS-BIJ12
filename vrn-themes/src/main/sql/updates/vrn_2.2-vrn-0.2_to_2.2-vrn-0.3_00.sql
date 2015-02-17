829ee456c416e159640b742bf86b69f4588e2fbf
Stefan van Wouw <stefan.van.wouw@geodan.nl>
2015-02-17 13:48:38 +0100

    Revert accidental commit of wrong attribute (doelbeheer instead of beheerpakket). beheerpakket was nullable already.

--- vrn-themes/src/main/sql/updates/vrn_2.2-vrn-0.2_to_2.2-vrn-0.3_00.sql
+++ vrn-themes/src/main/sql/updates/vrn_2.2-vrn-0.2_to_2.2-vrn-0.3_00.sql
@@ -1,7 +1,3 @@
-ALTER TABLE vrn.gebiedbeheer_landelijk ALTER COLUMN beheerpakket DROP NOT NULL;
-ALTER TABLE vrn.gebiedbeheer_provinciaal ALTER COLUMN beheerpakket DROP NOT NULL;
-ALTER TABLE vrn.gebiedbeheer_landelijk_tagged ALTER COLUMN beheerpakket DROP NOT NULL;
-
 
 -- Add constraints for tagged tables.
 alter table vrn.gebiedbeheer_landelijk_tagged add constraint enforce_dims_geometrie check (st_ndims(geometrie) = 2);
